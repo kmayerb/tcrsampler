74a37dee4e66370fe3fe75e30a7c9250f8df7a51
kmayerb <kmayerb@gmail.com>
2020-06-30 10:19:02 -0700

    bump version to 0.1.6

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ opts = dict(name='tcrsampler',
             license='MIT',
             author='Koshlan Mayer-Blackwell',
             author_email='kmayerbl@fredhutch.org',
-            version='0.1.5',
+            version='0.1.6',
             packages=PACKAGES,
             include_package_data=True)
 
