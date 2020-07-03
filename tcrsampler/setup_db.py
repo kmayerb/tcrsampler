2b38db4de3fae2b5b0c6021e96a805bf2d6181c5
kmayerb <kmayerb@gmail.com>
2020-07-03 13:55:36 -0700

    Add tests

--- tcrsampler/setup_db.py
+++ tcrsampler/setup_db.py
@@ -10,23 +10,20 @@ select_files = ["wiraninha_sampler.zip",
                 "britanova_human_beta_t_cb.tsv.sampler.tsv.zip"]
 
 
-address = { "wiraninha_sampler.zip"                           : "https://www.dropbox.com/s/ily0td3tn1uc7bi/wiraninha_sampler.zip?dl=1",
-            
+address = { "wiraninha_sampler.zip"                           : "https://www.dropbox.com/s/ily0td3tn1uc7bi/wiraninha_sampler.zip?dl=1",          
             "ruggiero_mouse_sampler.zip"                      : "https://www.dropbox.com/s/yz8v1c1gf2eyzxk/ruggiero_mouse_sampler.zip?dl=1",
             "ruggiero_human_sampler.zip"                      : "https://www.dropbox.com/s/jda6qtemk65zlfk/ruggiero_human_sampler.zip?dl=1",
-
             "britanova_human_beta_t_cb.tsv.sampler.tsv.zip"   : "https://www.dropbox.com/s/87n5v2by80xhy1q/britanova_human_beta_t_cb.tsv.sampler.tsv.zip?dl=1",
             "emerson_human_beta_t_cmvneg.tsv.sampler.tsv.zip" : "https://www.dropbox.com/s/04mxrzw7f5wkg1x/emerson_human_beta_t_cmvneg.tsv.sampler.tsv.zip?dl=1",
-            
             "ruggiero_human_alpha_t.tsv.sampler.tsv.zip"      : "https://www.dropbox.com/s/9h84bzhd0asfym7/ruggiero_human_alpha_t.tsv.sampler.tsv.zip?dl=1",
-            "ruggiero_human_beta_t.tsv.sampler.tsv.zip"       :"https://www.dropbox.com/s/onr5lntmlm4fivi/ruggiero_human_beta_t.tsv.sampler.tsv.zip?dl=1",
+            "ruggiero_human_beta_t.tsv.sampler.tsv.zip"       : "https://www.dropbox.com/s/onr5lntmlm4fivi/ruggiero_human_beta_t.tsv.sampler.tsv.zip?dl=1",
             "new_nextgen_chains_mouse_A.tsv"                  : 'https://www.dropbox.com/s/pkpr6p97eworn3q/new_nextgen_chains_mouse_A.tsv?dl=1',
             "new_nextgen_chains_mouse_B.tsv"                  : 'https://www.dropbox.com/s/sxgvrj25mnzr20s/new_nextgen_chains_mouse_B.tsv?dl=1',
             "new_nextgen_chains_human_A.tsv"                  : 'https://www.dropbox.com/s/9p43c7tscf46dat/new_nextgen_chains_human_A.tsv?dl=1',
             "new_nextgen_chains_human_B.tsv"                  : 'https://www.dropbox.com/s/83xbk7jp4xd3pr0/new_nextgen_chains_human_B.tsv?dl=1',
             "new_nextgen_chains_human_G.tsv"                  : 'https://www.dropbox.com/s/41w8yl38nr4ey32/new_nextgen_chains_human_A.tsv?dl=1',
             "new_nextgen_chains_human_D.tsv"                  : 'https://www.dropbox.com/s/8ysciqrcywdsryp/new_nextgen_chains_human_B.tsv?dl=1'}
-            
+
 
 def install_nextgen_data_to_db(download_file, download_from = "dropbox", dry_run = False):
     """
@@ -53,9 +50,9 @@ def install_nextgen_data_to_db(download_file, download_from = "dropbox", dry_run
     if download_from not in download_sources:
         raise KeyError(f"the <download_from> arg must be be one of the following {download_sources}")
 
-    if download_from is "dropbox":
+    if download_from is download_sources:
         if download_file not in address.keys():
-            raise KeyError("download_file must be in {}".format(" ".join(map(str,address.keys))))
+            raise KeyError("download_file must be in {}".format(",".join(map(str,address.keys()))))
 
     # Where the file is to be installed
     path_file = os.path.realpath(__file__)
--- tcrsampler/tests/test_setup_db.py
+++ tcrsampler/tests/test_setup_db.py
@@ -6,7 +6,7 @@ def test_install_all_next_gen(dry_run = True):
 	setup_db.install_all_next_gen(dry_run = dry_run)
 
 def test_install_nextgen_data_to_db_KeyError_download_from(download_file = "new_nextgen_chains_mouse_A.tsv", 
-									download_from = "fhcrc", 
+									download_from = "wrong_from", 
 									dry_run = True):
 	""" with dry_run set to true, test setup_db routine runs without error"""
 	with pytest.raises(KeyError):
@@ -15,7 +15,7 @@ def test_install_nextgen_data_to_db_KeyError_download_from(download_file = "new_
 											dry_run = dry_run)
 
 def test_install_nextgen_data_to_db_KeyError_filename(download_file = "new_nextgen_chains_hamster_A.tsv", 
-									download_from = "fhcrc", 
+									download_from = "dropbox", 
 									dry_run = True):
 	""" with dry_run set to true, test setup_db routine runs without error"""
 	with pytest.raises(KeyError):
@@ -23,7 +23,7 @@ def test_install_nextgen_data_to_db_KeyError_filename(download_file = "new_nextg
 											download_from = download_from , 
 											dry_run = dry_run)
 
-def test_install_nextgen_data_to_db_TypeError(download_file = ["new_nextgen_chains_mouse_A.tsv"], 
+def test_install_nextgen_data_to_db_TypeError(download_file = 1, 
 									download_from = "dropbox", 
 									dry_run = True):
 	""" with dry_run set to true, test setup_db routine runs without error"""
@@ -34,4 +34,12 @@ def test_install_nextgen_data_to_db_TypeError(download_file = ["new_nextgen_chai
 		assert e.type is TypeError
 		assert e.value.args[0].startswith('The <download_from> arg must be a string')
 
+
+def test_install_nextgen_dry_run_true(download_file = 'ruggiero_mouse_sampler.zip', 
+									  download_from = "dropbox", 
+									  dry_run = False):
+	""" with dry_run set to true, test setup_db routine runs without error"""
+	setup_db.install_nextgen_data_to_db(download_file = download_file, 
+										download_from = download_from , 
+										dry_run = dry_run)
 
